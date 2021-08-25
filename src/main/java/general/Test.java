55d214b29eb7c53453b063525cf847dfb676e445
ranesaci <ranesaci@gmail.com>
2021-08-25 15:24:17 +0530

    test

--- src/main/java/general/Test.java
+++ src/main/java/general/Test.java
@@ -9,6 +9,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("hello!!!");
+		double d1 = 374.56;
+		double d2 = 374.26;
+		System.out.println( "d1 - d2 = " + ( d1 - d2 ));
 
 	}
 
