ec261c3898d4be460aa99d4a9090e11abd3b85a2
Sachin Rane <ranesaci@gmail.com>
2022-09-13 18:08:58 +0530

    bubble sort

--- src/main/java/interviews/BubbleSort.java
+++ src/main/java/interviews/BubbleSort.java
@@ -1,36 +1,38 @@
 package interviews;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 
-/**
-* @author sachin
-* @date 19-Oct-2021
-*/
+import java.util.Arrays;
 
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		int[] arr = {0, 1, 2, 1, 1, 2, 0, 1};
+		int[] intArr = {47, 85, 62, 34, 7, 10, 92, 106, 2, 54};
+		System.out.println("Given Array" + Arrays.toString(intArr));
+		//boolean reverse = true;
+		boolean reverse = false;
 		
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = i; j < arr.length; j++) {
-				if (arr[i] > arr [j]) {
-					int temp = arr[j];
-					arr[j] = arr[i];
-					arr[i] = temp;
-					
+		for (int i = 0; i < intArr.length-1; i++) {
+			for (int j = i+1; j < intArr.length; j++) {
+				
+				if (reverse == true) {
+					if (intArr[i] < intArr[j]) {
+						swap(intArr,i,j);
+					}
+				} else {
+					if (intArr[i] > intArr[j]) {
+						swap(intArr,i,j);
+					}
 				}
 			}
-			
 		}
 		
-		for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
-		}
+		System.out.println("Sorted Array" + Arrays.toString(intArr));
+	}
 
+	private static void swap(int[] intArr, int i, int j) {
+		int temp = intArr[i];
+		intArr[i] = intArr[j];
+		intArr[j] = temp;
+		
 	}
 
 }
-
-
